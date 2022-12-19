0cb122d599aadcfd2267981861c98ac92975ead2
fran-bnd <fran-bnd@hotmail.com>
2022-12-19 23:05:58 +0000

    Lern about Java System out in and Scanner

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);  //Reading from System.in
+        System.out.println("Input a value: "); 
+        int number = sc.nextInt();
+        System.out.println("You inputted " + number); 
+        if (number > 0){
+            System.out.println("Positive number inputted");
+        }else if (number < 0){
+            System.out.println("Negative number inputted");
+        }else{
+            System.out.println("Number cero inputted"); 
+        }
+        sc.close();  //Closing scanner
+    }
+}
